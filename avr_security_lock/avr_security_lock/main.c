7cf4f96a9f86e1425ea94f51622b8877eb00cb33
Mustafa <mustafa.bahaa123@gmail.com>
2022-02-16 01:29:58 +0200

    add new atmel studio project

---
+++ avr_security_lock/avr_security_lock/main.c
@@ -0,0 +1,18 @@
+/*
+ * avr_security_lock.c
+ *
+ * Created: 2/16/2022 12:50:54 AM
+ * Author : Mustafa
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
